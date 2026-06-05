ce76e8277c6df78c7938d65c6752e06b3f1559d1
Junior Cabrera <cjjuniorstore@gmail.com>
2026-06-05 05:23:44 -0400

    Fix syntax error app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os from flask import Flask, render_template, request, redirect, url_for, flash
+import os 
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 
